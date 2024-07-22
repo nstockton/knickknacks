5464ee8c159a6774b9108626efc2b55f2050a9a0
Nick Stockton <nstockton@users.noreply.github.com>
2024-07-21 21:27:20 -0700

    Initial import.

---
+++ src/knickknacks/typedef.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2024 Nick Stockton
+# -----------------------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# -----------------------------------------------------------------------------
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# -----------------------------------------------------------------------------
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+import sys
+from typing import TypeVar
+
+
+if sys.version_info < (3, 10):  # pragma: no cover
+	from typing_extensions import TypeAlias
+else:  # pragma: no cover
+	from typing import TypeAlias
+
+if sys.version_info < (3, 9):  # pragma: no cover
+	from typing import Pattern
+else:  # pragma: no cover
+	from re import Pattern
+
+
+BytesOrStr = TypeVar("BytesOrStr", bytes, str)
+REGEX_PATTERN: TypeAlias = Pattern[str]
